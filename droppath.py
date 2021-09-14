ba8c614adedbbd3341d29ced6e4bd8fcf6c181a9
tai-dang11 <78414238+tai-dang11@users.noreply.github.com>
2021-09-13 23:47:53 -0400

    Update droppath.py

--- droppath.py
+++ droppath.py
@@ -1,16 +1,12 @@
-
 import tensorflow as tf
-
 
 def drop_path(inputs, drop_prob, is_training):
     if (not is_training) or (drop_prob == 0.):
         return inputs
 
-    # Compute keep_prob
     keep_prob = 1.0 - drop_prob
-
-    # Compute drop_connect tensor
     random_tensor = keep_prob
+
     shape = (inputs.shape[0],) + (1,) * \
         (tf.experimental.numpy.ndim(inputs) - 1)
     random_tensor += tf.random.uniform(shape, dtype=inputs.dtype)
